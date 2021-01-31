60b563b65ade30ec4ce3e80a21e161647b70854b
kimyb8870 <qhd0084@gmail.com>
2021-02-01 04:40:50 +0900

    Add: firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCil0Vs7mRGwTg49xNq7Kob6SxfbBrviHo",
+  authDomain: "c-est-la-vie-3c792.firebaseapp.com",
+  projectId: "c-est-la-vie-3c792",
+  storageBucket: "c-est-la-vie-3c792.appspot.com",
+  messagingSenderId: "915351414705",
+  appId: "1:915351414705:web:513aa8390b2a066e6dff90",
+  measurementId: "G-N01SHQB4E4",
+};

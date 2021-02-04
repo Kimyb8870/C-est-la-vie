4f1dc2ff6467fa1fc35029a53ac5fb05a05de1c1
kimyb8870 <qhd0084@gmail.com>
2021-02-05 03:22:13 +0900

    Feature: mainpage header

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,13 +1,15 @@
 import React from "react";
 import MainPageHeader from "./MainPageHeader";
+import "./MainPage.css";
 
 const MainPage = ({ user }) => {
   return (
     <div className="mainpage">
       <MainPageHeader />
-      <section className="mainpage__body"></section>
+      <section className="mainpage__body">mainpage body</section>
       <footer className="mainpage__footer">
         {/* mainpage header */}
+        mainpage footer
         {/* app button to go top */}
       </footer>
     </div>
--- src/pages/MainPage/MainPageHeader.js
+++ src/pages/MainPage/MainPageHeader.js
@@ -1,48 +1,22 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
+import "./MainPageHeader.css";
 import MenuIcon from "@material-ui/icons/Menu";
-import "../../assets/images/logo.svg";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    margin: 5,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import PersonIcon from "@material-ui/icons/Person";
+import { IconButton } from "@material-ui/core";
 
 const MainPageHeader = () => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            <img src={require("../../assets/images/logo.svg")} />
-          </Typography>
-          <Button color="inherit">Profile</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <nav className="mainpage__header">
+      <div className="mainpage__header__navGroup">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        {/* 로고는 나중에 */}
+        <IconButton>
+          <PersonIcon />
+        </IconButton>
+      </div>
+    </nav>
   );
 };
 
